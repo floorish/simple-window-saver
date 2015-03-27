1f12d8c18c822276ddb8bf3554d6e9fcd38d18c1
floorish <floor@iMac.local>
2015-03-27 17:09:38 +0100

    Removed old localStorage script

--- localStorage.js
+++
@@ -1,9 +0,0 @@
-// helper function to restore from localStorage
-function restoreFromLocalStorage(key, defaultValue) {
-  if (localStorage[key]) {
-    return JSON.parse(localStorage[key]);
-  } else {
-    localStorage[key] = JSON.stringify(defaultValue);
-    return defaultValue;
-  }
-}
